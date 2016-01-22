f3edc18ecf888be2a6609e9c6c4fee282d5e1ff1
edagarli <lizhi@lydiabox.com>
2016-01-22 11:25:44 +0800

    init framework

---
+++ src/main/java/org/smart4j/framework/ConfigConstant.java
@@ -0,0 +1,27 @@
+package org.smart4j.framework;
+
+/**
+ * User: lurou
+ * Email: lurou@2dfire.com
+ * Date: 16/1/21
+ * Time: 23:54
+ * Desc:
+ */
+public interface ConfigConstant {
+
+    String CONFIG_FILE = "smart.properties";
+
+    String JDBC_DRIVER = "smart.framework.jdbc.driver";
+
+    String JDBC_URL = "smart.framework.jdbc.url";
+
+    String JDBC_USERNAME = "smart.framework.jdbc.username";
+
+    String JDBC_PASSWORD = "smart.framework.jdbc.password";
+
+    String APP_BASE_PACKAGE = "smart.framework.app.base_package";
+
+    String APP_JSP_PATH = "smart.framework.app.jsp_path";
+
+    String APP_ASSET_PATH = "smart.framework.app.asset_path";
+}
---
+++ src/main/java/org/smart4j/framework/util/PropsUtil.java
@@ -0,0 +1,22 @@
+package org.smart4j.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+/**
+ * User: lurou
+ * Email: lurou@2dfire.com
+ * Date: 16/1/22
+ * Time: 00:23
+ * Desc:
+ */
+public class PropsUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+
+    public static Properties loadProps(String fileName){
+
+        return null;
+    }
+}
